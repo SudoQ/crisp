d73aae88d99df7a2abe32d1f4bbf4610fa73818c
SudoQ <simj91@gmail.com>
2015-10-31 18:25:57 +0100

    Service package

---
+++ service/service.go
@@ -0,0 +1,169 @@
+package service
+
+import (
+	"github.com/SudoQ/ganache/item"
+	"github.com/gorilla/mux"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"time"
+	"net/http"
+	"fmt"
+)
+
+type Service struct {
+	URL    string `json:"url"`
+	Port   string `json:"port"`
+	Period time.Duration `json:"-"`
+	Limit  int `json:"limit"`
+	Cache  *item.Item `json:"-"`
+}
+
+func New(url, port string, limit int) *Service {
+	period := LimitToDuration(limit)
+	return &Service{
+		URL:    url,
+		Port:   port,
+		Period: period,
+		Limit:  limit,
+		Cache:  nil,
+	}
+}
+
+func LimitToDuration(limit int) time.Duration {
+	return time.Duration(time.Duration(limit/60) * time.Minute)
+}
+
+func NewFromJSON(jsonBlob []byte) (*Service, error) {
+	var srv Service
+	err := json.Unmarshal(jsonBlob, &srv)
+	if err != nil{
+		return nil, err
+	}
+	srv.Period = LimitToDuration(srv.Limit)
+	return &srv, nil
+}
+
+func NewFromFile(filename string) (*Service, error) {
+	jsonBlob, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return NewFromJSON(jsonBlob)
+}
+
+func (this *Service) LoadConfig(filename string) error {
+	jsonBlob, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(jsonBlob, this)
+	return err
+}
+
+func (this *Service) JSON() ([]byte, error) {
+	// Return service configuration as JSON
+	blob, err := json.Marshal(this)
+	if err != nil {
+		return nil, err
+	}
+	return blob, nil
+}
+
+func (this *Service) Chaos(err error) bool {
+	if err != nil {
+		// TODO Log error
+		log.Println(err)
+		return true
+	}
+	return false
+}
+
+func (this *Service) SaveConfig(filename string) error {
+	jsonBlob, err := this.JSON()
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filename, jsonBlob, 0644)
+	return err
+}
+
+func (this *Service) Collect() {
+	for {
+		now := time.Now()
+		timeDelta := now.Sub(this.Cache.Timestamp)
+		if this.Cache.Timestamp.Before(now) && timeDelta > this.Period {
+			log.Printf("GET %s\n", this.URL)
+
+			resp, err := http.Get(this.URL)
+			if this.Chaos(err) {
+				// TODO handle error
+				log.Fatal(err)
+			}
+
+			payload, err := ioutil.ReadAll(resp.Body)
+			if this.Chaos(err) {
+				// TODO handle error
+				log.Fatal(err)
+			}
+
+			// TODO Store header info?
+			newItem := item.New(now, payload)
+			this.Cache = newItem
+			cacheFilename := "cache.json"
+			err = this.Cache.WriteFile(cacheFilename)
+			if this.Chaos(err){
+				log.Fatal(err)
+			}
+			log.Printf("Saved cache to %s\n", cacheFilename)
+
+		}
+		select {
+		case <-time.After(this.Period):
+			continue
+		}
+	}
+}
+
+func (this *Service) LoadCache(filename string) error {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil{
+		return err
+	}
+	newItem, err := item.NewFromJSON(content)
+	if err != nil {
+		return err
+	}
+	this.Cache = newItem
+	return nil
+}
+
+func (this * Service) Info() string {
+	return fmt.Sprintf("Ganache API caching service v0.1")
+}
+
+func (this *Service) Run() {
+	err := this.LoadCache("cache.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go this.Collect()
+	r := mux.NewRouter()
+	r.HandleFunc("/", this.HomeHandler)
+	r.HandleFunc("/cache.json", this.CacheHandler)
+	port := fmt.Sprintf(":%s", this.Port)
+	http.ListenAndServe(port, r)
+}
+
+func (this *Service) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(this.Info()))
+}
+
+func (this *Service) CacheHandler(w http.ResponseWriter, r *http.Request) {
+	response, err := this.Cache.JSON()
+	if err != nil {
+		// TODO
+		log.Fatal(err)
+	}
+	w.Write(response)
+}
